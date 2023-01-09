91c3d79bcd7e1077f42b0565e9c76a090928c924
Hannes Mann <hannesmann2000@gmail.com>
2023-01-09 22:18:58 +0100

    Fix ensembles.sql

--- soundgood/queries/ensembles.sql
+++ soundgood/queries/ensembles.sql
@@ -4,7 +4,10 @@
 DROP MATERIALIZED VIEW IF EXISTS ensemble_overview_next_week CASCADE;
 
 CREATE VIEW ensembles_next_week AS 
-	SELECT ensemble.* 
+	SELECT 
+		ensemble.*,
+		EXTRACT(DAY from time_slot.date_and_time) + 1 as week_day,
+		TO_CHAR(time_slot.date_and_time, 'Dy') as week_day_str
 	FROM ensemble
 
 	LEFT JOIN lesson ON lesson.id = ensemble.lesson_id
@@ -24,12 +27,32 @@ CREATE VIEW ensembles_students AS
 
 CREATE MATERIALIZED VIEW ensemble_overview_next_week AS
 	SELECT 
-		*,
-		(ensembles_sum.max_students = ensembles_sum.attending) AS full,
-		(ensembles_sum.max_students - ensembles_sum.attending <= 2) AS soon_full
+		ensembles_sum.lesson_id,
+		ensembles_sum.genre,
+		ensembles_sum.week_day_str as week_day,
+
+		ensembles_sum.min_students,
+		ensembles_sum.max_students,
+		ensembles_sum.attending,
+
+		CASE 
+			WHEN (ensembles_sum.max_students = ensembles_sum.attending) THEN 'Full'
+			WHEN (ensembles_sum.max_students - ensembles_sum.attending <= 2) THEN '1-2 seats left'
+			ELSE 'Several seats left'
+		END status
 
 		FROM (
-			SELECT *
+			SELECT 
+				ensembles_next_week.lesson_id as lesson_id,
+				ensembles_next_week.min_students as min_students,
+				ensembles_next_week.max_students as max_students,
+				ensembles_next_week.genre as genre,
+				ensembles_students.attending as attending,
+				ensembles_next_week.week_day as week_day,
+				ensembles_next_week.week_day_str as week_day_str
+
 			FROM ensembles_next_week
 			LEFT JOIN ensembles_students ON ensembles_students.lesson_id = ensembles_next_week.lesson_id
-		) as ensembles_sum;
+		) as ensembles_sum
+
+		ORDER BY ensembles_sum.genre, ensembles_sum.week_day;
